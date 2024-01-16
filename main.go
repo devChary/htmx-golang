e856c15f4a5deb60338db899c68e9fc8cb3d7810
devChary <jagannadhachary7182@gmail.com>
2024-01-16 13:26:41 +0530

    feat: setting up server and serving html

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Ello Mate!")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("index.html"))
+		template.Execute(w, nil)
+	}
+	http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
